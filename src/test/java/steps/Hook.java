9c017dc57244dd07d3d03bfdb4827245991131b9
sureshpenamati <suresh.sssu@gmail.com>
2017-12-15 15:09:55 -0800

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "//Users//sureshpenamati//Documents//Drivers//chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/sureshpenamati/Documents/Drivers/chromedriver");
         base.Driver = new ChromeDriver();
     }
 
